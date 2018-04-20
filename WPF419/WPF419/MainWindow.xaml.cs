3c9751ddb5c8c74f51d80c443946caaf4a267498
J102-STUDENT31\Student <lulaw03@gmail.com>
2018-04-20 12:40:14 +0800

    third commit 420

---
+++ WPF419/WPF419/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF419
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // 宣告身高(cm)和體重(kg)的變數
+            int height = 158;
+            int weight = 60;
+            int BMI;
+
+            // 計算BMI
+            BMI = weight / (height/100) * (height/100);
+
+            // 顯示bmi數值
+            Console.ReadLine();
+        }
+    }
+}
